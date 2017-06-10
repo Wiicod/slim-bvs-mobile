1f9514c009bbd14433dbc4e08763bab62472e651
Edward NANDA <edward.nanda@wiicod.com>
2017-06-10 17:50:52 +0100

    Integration google map

--- www/components/client/client.js
+++ www/components/client/client.js
@@ -4,6 +4,36 @@
 
 app
 
-    .controller("UniversCtrl",['$scope',function($scope){
+    .controller("UniversCtrl",['$scope','NgMap','$rootScope',function($scope,NgMap,$rootScope){
         $scope.current=new Date();
+        // douala 4.0526383,9.6973306
+        $scope.position=null;
+        $scope.lat=null;
+        $scope.lng=null;
+
+        NgMap.getMap().then(function(map) {
+            $rootScope.map = map;
+            // Try HTML5 geolocation.
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    var pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    };
+                    $scope.position=pos;
+                    $scope.lat=pos.lat;
+                    $scope.lng=pos.lng;
+                    console.log($scope.lng,$scope.lat);
+                    map.setCenter(pos);
+                }, function() {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                });
+            } else {
+                // Browser doesn't support Geolocation
+                handleLocationError(false, infoWindow, map.getCenter());
+            }
+            var center = map.getCenter();
+            google.maps.event.trigger(map, "resize");
+            map.setCenter(center);
+        });
     }])
